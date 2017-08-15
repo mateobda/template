73b10c8f9e0922c265ea991384c14c1a577a6ca7
Mateo Olarte <mateo.programacion3@gmail.com>
2017-08-15 16:23:44 -0500

    [update] finished word search feature and fixed import task gulpfile

--- BDA/js/dev/base.js
+++ BDA/js/dev/base.js
@@ -1,6 +1,13 @@
 function init() {
-$("#bda-word-search__content").wordSearch({
-    dificulty: 10,
-    words: ['cultura', 'paz', 'mundo',]
+  $("#bda-word-search__content").wordSearch({
+    dificulty: 6,
+    words: ['cultura', 'paz'],
+    colorWord: "#d46919"
+  })
+
+  $("#bda-word-search__content-1").wordSearch({
+    dificulty: 6,
+    words: ['cultura', 'paz'],
+    colorWord: "#B71017"
   })
 }
--- BDA/js/index.js
+++ BDA/js/index.js
@@ -1,2 +1,2 @@
-"use strict";function init(){$("#bda-word-search__content").wordSearch({dificulty:10,words:["cultura","paz","mundo"]})}
+"use strict";function init(){$("#bda-word-search__content").wordSearch({dificulty:6,words:["cultura","paz"],colorWord:"#d46919"}),$("#bda-word-search__content-1").wordSearch({dificulty:6,words:["cultura","paz"],colorWord:"#B71017"})}
 //# sourceMappingURL=index.js.map
