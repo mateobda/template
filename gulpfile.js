a8a4f3afc8f70234d8a497e971a39f8a9872bd35
Mateo Olarte <mateo.programacion3@gmail.com>
2017-09-20 12:08:01 -0500

    [update] create bundle with custom and vendors styles, reestructure gulp build task, delete folder viewer

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,25 @@
-// const gulp = require('gulp')
-// const uglify = require('gulp-uglify')
-// const imagemin = require('gulp-imagemin')
-// const cache = require('gulp-cache')
-// const autoprefixer = require('gulp-autoprefixer')
-// const babel = require('gulp-babel')
-// const htmlmin = require('gulp-htmlmin')
-// const cssmin = require('gulp-cssmin')
-// const cssimport = require("gulp-cssimport")
-
 const pkg = require('./package.json')
 const gulp = require('gulp')
-const $ = require('gulp-load-plugins')({
-  pattern: ["*"],
-  scope: ["devDependencies"]
-})
+const $ = require('gulp-load-plugins')({ pattern: ["*"], scope: ["devDependencies"] })
 const browserSync = require('browser-sync').create()
 const runSequence = require('run-sequence')
 const path = require('path')
 const nameOVA = path.basename(__dirname)
 
-gulp.task('vendors', () => {
-  return gulp.src('common/base/css/_vendors/vendors.css')
-  .pipe($.cssimport())
-  .pipe($.cssmin())
-  .pipe(gulp.dest('common/base/css/'))
-})
-
-gulp.task('styles:common', () => {
-  return gulp.src('common/base/css/_styles/main.css')
-  .pipe($.cssimport())
-  .pipe($.cssmin())
-  .pipe(gulp.dest('common/base/css/'))
+gulp.task('styles', () => {
+  return gulp.src('./common/base/css/main.css')
+    .pipe($.cssimport())
+    .pipe($.autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
+    }))
+    .pipe($.cssmin())
+    .pipe($.rename('bundle.css'))
+    .pipe(gulp.dest('common/base/css/'))
 })
 
 gulp.task('default', () => {
   browserSync.init({
-    server: {
-      baseDir: '.'
-    },
+    server: { baseDir: '.' },
   })
 
   gulp.watch(['*.html', 'content/*.html', 'common/**/*'], browserSync.reload)
@@ -45,55 +27,93 @@ gulp.task('default', () => {
 
 gulp.task('html:content', () => {
   return gulp.src(['content/*.html', 'content/navigation.json'])
-  .pipe($.htmlmin({
-    collapseWhitespace: true,
-    minifyCSS: true,
-    minifyJS: {compress: {drop_console: true}},
-    processConditionalComments: true,
-    removeComments: true,
-    removeEmptyAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true
-  }))
-  .pipe(gulp.dest(`../${nameOVA}_build/content`))
-})
-
-gulp.task('html:viewer', () => {
-  return gulp.src('index.html')
-  .pipe($.htmlmin({
-    collapseWhitespace: true,
-    minifyCSS: true,
-    minifyJS: {compress: {drop_console: true}},
-    processConditionalComments: true,
-    removeComments: true,
-    removeEmptyAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true
-  }))
-  .pipe(gulp.dest(`../${nameOVA}_build/`))
+    .pipe($.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: {
+        compress: {
+          drop_console: true
+        }
+      },
+      processConditionalComments: true,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true
+    }))
+    .pipe(gulp.dest(`../${nameOVA}_build/content`))
 })
 
 gulp.task('images:prod', () => {
   return gulp.src('images/**/*')
-  .pipe($.cache($.imagemin()))
-  .pipe(gulp.dest(`../${nameOVA}_build/images`))
+    .pipe($.cache($.imagemin()))
+    .pipe(gulp.dest(`../${nameOVA}_build/images`))
 })
 
 gulp.task('iframes:prod', () => {
   return gulp.src('iframes/**/*')
-  .pipe(gulp.dest(`../${nameOVA}_build/iframes`))
+    .pipe(gulp.dest(`../${nameOVA}_build/iframes`))
 })
 
 gulp.task('audios:prod', () => {
   return gulp.src('audio/**/*')
-  .pipe(gulp.dest(`../${nameOVA}_build/audio`))
+    .pipe(gulp.dest(`../${nameOVA}_build/audio`))
 })
 
-gulp.task('common:prod', () => {
-  return gulp.src('common/**/*')
-  .pipe(gulp.dest(`../${nameOVA}_build/common`))
+gulp.task('html:viewer', () => {
+  return gulp.src('index.html')
+    .pipe($.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: {
+        compress: {
+          drop_console: true
+        }
+      },
+      processConditionalComments: true,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true
+    }))
+    .pipe(gulp.dest(`../${nameOVA}_build/`))
+})
+
+gulp.task('styles:prod', () => {
+  return gulp.src('./common/base/css/bundle.css')
+    .pipe(gulp.dest(`../${nameOVA}_build/common/base/css/`))
+})
+
+gulp.task('scripts:viewer', () => {
+  return gulp.src('./common/base/js/viewer.js')
+    .pipe($.babel({ presets: ['env'] }))
+    .pipe($.uglify())
+    .pipe(gulp.dest(`../${nameOVA}_build/common/base/js/`))
+})
+
+gulp.task('scripts:main', () => {
+  return gulp.src('./common/base/js/main.js')
+    .pipe(gulp.dest(`../${nameOVA}_build/common/base/js/`))
+})
+
+gulp.task('common:base', ['styles:prod', 'scripts:viewer', 'scripts:main'], () => {
+  return gulp.src(['./common/base/*/*', '!./common/base/js/*', '!./common/base/css/*'])
+    .pipe(gulp.dest(`../${nameOVA}_build/common/base`))
+})
+
+gulp.task('common:prod', ['common:base'], () => {
+  return gulp.src(['./common/*/*', '!./common/base/**/*'])
+    .pipe(gulp.dest(`../${nameOVA}_build/common/`))
 })
 
 gulp.task('build', (callback) => {
-  runSequence(['common:prod', 'html:content', 'html:viewer', 'images:prod', 'audios:prod', 'iframes:prod'], callback)
+  runSequence([
+    'common:prod',
+    'html:content',
+    'html:viewer',
+    'images:prod',
+    'audios:prod',
+    'iframes:prod'
+  ],
+  callback)
 })
