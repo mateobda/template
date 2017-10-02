db13072bd8ef8ce7c7bf7b438f71518c8ebff691
Mateo Olarte <mateo.programacion3@gmail.com>
2017-10-02 16:41:04 -0500

    [fixed] fixed url icon humanidades

--- common/base/js/viewer.js
+++ common/base/js/viewer.js
@@ -21,33 +21,31 @@ $(document).on('click', '.main-nav__tab', function () {
 
 $(document).on('ready', () => {
   fetch('content/navigation.json')
-  .then(data => data.json())
-  .then(data => {
-    buildMenu(data)
+    .then(data => data.json())
+    .then(data => {
+      buildMenu(data)
 
-    const firstContent = `content/${$('.main-nav').find("[data-nav='content-1']").data("ova")}`
-    $.get(firstContent, (data, status) => $("#stage").html(data))
+      const firstContent = `content/${$('.main-nav').find("[data-nav='content-1']").data("ova")}`
 
-    return data
-  })
-  .then(data => {
-    navigationOVA(data)
-  })
-  .catch(error => {
-    console.error(error)
-  })
+      $.get(firstContent, (data, status) => $("#stage").html(data))
+
+      return data
+    })
+    .then(data => navigationOVA(data))
+
+    .catch(error => console.error(error))
 
   const buildMenu = jsonInfo => {
     jsonInfo.menu.content.forEach((value, index) => {
 
       const itemContent =
-      `<li class="main-nav__item ${(value.order == 1) ? 'active' : ''}"><a href="" class="navigation" data-nav="content-${value.order}" data-ova="${value.link}">${value.title}</a></li>`
+        `<li class="main-nav__item ${(value.order == 1) ? 'active' : ''}"><a href="" class="navigation" data-nav="content-${value.order}" data-ova="${value.link}">${value.title}</a></li>`
       $('.content-tab').append(itemContent)
     })
 
     jsonInfo.menu.resume.forEach((value, index) => {
       const itemResume =
-      `<li class="main-nav__item"><a href="" class="navigation" data-nav="resume-${value.order}" data-ova="${value.link}">${value.title}</a></li>`
+        `<li class="main-nav__item"><a href="" class="navigation" data-nav="resume-${value.order}" data-ova="${value.link}">${value.title}</a></li>`
       $('.resume-tab').append(itemResume)
     })
   }
@@ -90,7 +88,7 @@ $(document).on('ready', () => {
       removeNavButtons(numRes)
     })
 
-    $(document).on('click', '.viewer_next', function () {
+    $(document).on('click', '.viewer_next', () => {
       const conType = $('.active').children().data("nav").split("-")[0]
       const contNum = parseInt($('.active').children().data("nav").split("-")[1])
 
@@ -123,50 +121,50 @@ $(document).on('ready', () => {
       }
     })
 
-    $(document).on('click', '.viewer_prev', function () {
+    $(document).on('click', '.viewer_prev', () => {
       const conType = $('.active').children().data("nav").split("-")[0]
       const contNum = parseInt($('.active').children().data("nav").split("-")[1])
 
-      if (((conType === 'content') && (contNum > 1)) ) {
-          $('.viewer_next').removeClass('off')
-          let theme = contNum - 1
-          theme = conType + '-' + (theme)
-          const file = `content/${$('.main-nav').find("[data-nav='"+ theme + "']").data("ova")}`
+      if (((conType === 'content') && (contNum > 1))) {
+        $('.viewer_next').removeClass('off')
+        let theme = contNum - 1
+        theme = conType + '-' + (theme)
+        const file = `content/${$('.main-nav').find("[data-nav='"+ theme + "']").data("ova")}`
 
-          $.get(file, (data, status) => $("#stage").html(data).promise().done(startPage()))
+        $.get(file, (data, status) => $("#stage").html(data).promise().done(startPage()))
 
-          $('.main-nav__item').removeClass('active')
-          $('.main-nav').find("[data-nav='" + theme + "']").parent().addClass('active')
-          if ((conType == 'content') && (contNum == 2)) {
-            $('.viewer_prev').addClass('off')
-          }
-
-        } else if ((conType == 'resume') && (contNum == 1)) {
-          var theme = 'content-' + numCont
-
-          $('.viewer_next').removeClass('off')
-          if (numCont==1) {
-            $('.viewer_prev').addClass('off')
-          }
-
-          toggleArrowNext()
-
-          $('.main-nav__item').removeClass('active')
-          $('.main-nav').find("[data-nav='" + theme + "']").parent().addClass('active')
-          const file = `content/${$('.main-nav').find("[data-nav='" + theme + "']").data("ova")}`
-
-          $.get(file, (data, status) => $("#stage").html(data).promise().done(startPage()))
+        $('.main-nav__item').removeClass('active')
+        $('.main-nav').find("[data-nav='" + theme + "']").parent().addClass('active')
+        if ((conType == 'content') && (contNum == 2)) {
+          $('.viewer_prev').addClass('off')
         }
-      })
-    }
-  })
 
-  $(document).on("click", ".navigation", function (e) {
-    e.preventDefault()
-    const file = `content/${$(this).data("ova")}`
+      } else if ((conType == 'resume') && (contNum == 1)) {
+        var theme = 'content-' + numCont
 
-    $.get(file, (data, status) => $("#stage").html(data).promise().done(startPage()))
+        $('.viewer_next').removeClass('off')
+        if (numCont == 1) {
+          $('.viewer_prev').addClass('off')
+        }
 
-    $('#sidebar, #content_ova').removeClass('on')
-    $('.toogle-menu__item').addClass('off')
-  })
+        toggleArrowNext()
+
+        $('.main-nav__item').removeClass('active')
+        $('.main-nav').find("[data-nav='" + theme + "']").parent().addClass('active')
+        const file = `content/${$('.main-nav').find("[data-nav='" + theme + "']").data("ova")}`
+
+        $.get(file, (data, status) => $("#stage").html(data).promise().done(startPage()))
+      }
+    })
+  }
+})
+
+$(document).on("click", ".navigation", function (e) {
+  e.preventDefault()
+  const file = `content/${$(this).data("ova")}`
+
+  $.get(file, (data, status) => $("#stage").html(data).promise().done(startPage()))
+
+  $('#sidebar, #content_ova').removeClass('on')
+  $('.toogle-menu__item').addClass('off')
+})
